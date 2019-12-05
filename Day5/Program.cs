e293b77b329dd7741a91f47993c389f007bf2420
Timothy Griffiths <timgriffiths01@gmail.com>
2019-12-05 12:57:37 +0000

    Day 5: Fun with VMs! Most of the time was taken with debugging simple mistakes on my part.

---
+++ Day5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Day5
+{
+    class Program
+    {
+        static int[] program;
+        static IntCodeVM VM;
+        static void Main()
+        {
+            string[] input = File.ReadLines(@"./Programs/diagnostic.txt").First().Split(',');
+            program = Array.ConvertAll(input, int.Parse);
+            VM = new IntCodeVM(program);
+
+            while (true)
+            {
+                VM.LoadProgram(program);
+                VM.Execute();
+                Console.WriteLine("Program has Finished Executing");
+            }
+        
+        }
+
+
+    }
+}
