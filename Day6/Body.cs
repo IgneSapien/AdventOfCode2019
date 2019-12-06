de560e12701724db5a88aa42b37dc7973a2317a3
Timothy Griffiths <timgriffiths01@gmail.com>
2019-12-06 13:34:37 +0000

    Day 6: Fun with Trees

---
+++ Day6/Body.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Day6
+{
+    public class Body : IEnumerable<Body>
+    {
+        public string ID { get; }
+
+        public Body Parent { get; private set; }
+
+        private readonly List<Body> _children;
+
+        public Body (string id)
+        {
+            ID = id;
+            _children = new List<Body>();
+        }
+
+        public void Add(Body body)
+        {
+            if (body.Parent != null)
+            {
+                body.Parent._children.Remove(body);
+            }
+            body.Parent = this;
+            _children.Add(body);
+        }
+
+        public IEnumerable<Body> GetNodeAndDescendants() 
+        {
+            return new[] { this }
+                   .Concat(_children.SelectMany(child => child.GetNodeAndDescendants()));
+        }
+
+        public int ParentCount
+        {
+            get
+            {
+                if(Parent == null)
+                {
+                    return 0;
+                }
+
+                return 1 + Parent.ParentCount;
+            }
+        }
+
+        public int ChildrenCount
+        { get { return this._children.Count; } }
+
+        public IEnumerator<Body> GetEnumerator()
+        {
+            return _children.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+
+
+        public Body Find(string ID)
+        {
+            return GetNodeAndDescendants()
+                         .FirstOrDefault(node => node.ID == ID);
+        }
+
+        public int SumChild()
+        {
+            var nodes = GetNodeAndDescendants();
+
+            int r = 0;
+            foreach (Body body in nodes)
+            {
+                r += body.ParentCount;
+            }
+
+            return r;
+        }
+
+        public List<string> PathToRoot()
+        {
+            List<string> path = new List<string>();
+            if(Parent == null)
+            {
+                return path;
+            }
+            path.Add(Parent.ID);
+            path.AddRange(Parent.PathToRoot());
+            return path;
+        }
+
+        public void MakeRoot()
+        {
+            this.Parent = null;
+        }
+    }
+}
---
+++ Day6/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Day6
+{
+    class Program
+    {
+        static List<string[]> bodies;
+        static Body root;
+        static void Main()
+        {
+            //FOR PART2
+            //Should I find the first common orbit, rebase the tree and then sum the jumps from each? Do
+            //If I move the county stuff into the nodes no need to rebase
+
+            var input = File.ReadLines("Task1.txt").ToList();
+
+            bodies = new List<string[]>();
+            foreach (string s in input)
+            {
+                var p = s.Split(')');
+                bodies.Add(p);
+            }
+
+            root = new Body("COM");
+            BuildTree("COM");
+
+            //Console.WriteLine(root.SumChild().ToString());
+
+
+            var youPath = root.Find("YOU").PathToRoot();
+            var sanPath = root.Find("SAN").PathToRoot();
+            var intersect = youPath.Intersect(sanPath);
+
+            //foreach(string s in intersect)
+            //{
+            //    Console.WriteLine(s);
+            //}
+
+            Body newRoot = root.Find(intersect.First());
+
+            newRoot.MakeRoot();
+
+            int r = newRoot.Find("YOU").SumChild();
+            r += newRoot.Find("SAN").SumChild();
+            //This method includes "jumps" to YOU and SANs so we'd be off by 2.
+            r -= 2;
+            Console.WriteLine(r.ToString());
+
+            Console.ReadLine();
+        }
+
+        private static void BuildTree(string ID)
+        {
+            var children = bodies.Where(b => b[0] == ID);
+            Body parent = root.Find(ID);
+            foreach (string[] child in children)
+            {
+                parent.Add(new Body(child[1]));
+                BuildTree(child[1]);
+            }
+        }
+    }
+}
