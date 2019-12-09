e72372d85448584dd00e1f84e4a77f37a7245b63
Timothy Griffiths <timgriffiths01@gmail.com>
2019-12-09 16:56:43 +0000

    Day9: Refactored to make debugiging the the [203, 0] Error easier

--- Day9/IntCodeVM.cs
+++ Day9/IntCodeVM.cs
@@ -3,6 +3,7 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Day9
@@ -13,48 +14,57 @@ namespace Day9
 
     public class IntCodeVM
     {
-        //Thought it would be fun to treat the VM a bit more like a actual computer with regeistesr etc.
-        long a;
-        long b;
-        long bus;
         long pCounter;
         long pOffset;
+
         bool run;
+
+        public bool InputRequired { get; private set; }
 
         //Working memory, wiped when a program is loaded
         public long[] Memory { get; private set; }
 
         //A list of programs, currently not required but acts like having the programs saved to a disk.
+        //Might want to turn this to a dictoanry 
         List<long[]> programs;
 
         //Input/Output queues that are thread safe(?)
-        public BlockingCollection<long> Input { get; private set; }
+        BlockingCollection<long> Input;
         public BlockingCollection<long> Output { get; private set; }
 
-        public IntCodeVM(long[] defaultProgram)
+        public IntCodeVM(List<long[]> intialPrograms)
         {
             run = false;
+            InputRequired = false;
             Input = new BlockingCollection<long>();
             Output = new BlockingCollection<long>();
-            Clear();
-            programs = new List<long[]>
-            {
-                defaultProgram
-            };
+            programs = intialPrograms;
             //Not sure we should load the program here, I'll often want to run the VM on a loop to try diffrent inputs so it needs loading before running each time.
             //LoadProgram();
         }
 
         //Whipes memory and loads a program at a given index of the program list into the start of the memeory array 
+        //TODO: Need to handle trying to load programs that don't exist. 
         public void LoadProgram(int programNumber = 0)
         {
             //Arrays can only be a max of 2GB unless we're x64 and set <gcAllowVeryLargeObjects enabled="true" /> 
             //Not sure how much memeory we actually need but it's possible we'll get out of range exceptions 
             //5000 is more than enough for the dignotistic program
             //Not sure if it's effcent to crate a new array everytime but should be safe.
-            Memory = new long[10000];
+            Memory = new long[50000000];
             long[] program = programs[programNumber];
             Array.Copy(program, Memory, program.Length);
+        }
+
+        public void AddProgram(long[] program)
+        {
+            programs.Add(program);
+        }
+
+        public void AddInput(long input)
+        {
+            InputRequired = false;
+            Input.Add(input);
         }
 
 
@@ -62,14 +72,11 @@ namespace Day9
         //Runs as an async task so the program can wait on any required inputs with out blocking.
         public Task Execute()
         {
-            Clear();
             pCounter = 0;
             pOffset = 0;
             run = true;
             while (run)
             {
-                //just to be on the safe side
-                Clear();
                 //Instructions are encoded [param3][param2][param1][[oppcode]] but with leading zeros removed
                 //This puts the zeros in so we can treat all instructions the same way
                 string code = Memory[pCounter].ToString("00000");
@@ -84,7 +91,7 @@ namespace Day9
                 {
                     case "01": OppSum(param); break;
                     case "02": OppMultiply(param); break;
-                    case "03": OppInput(); break;
+                    case "03": OppInput(param); break;
                     case "04": OppOutput(param); break;
                     case "05": OppJumpTrue(param); break;
                     case "06": OppJumpFalse(param); break;
@@ -101,13 +108,6 @@ namespace Day9
             return Task.CompletedTask;
         }
 
-        //Clears up the regiester and bus, shouldn't stricntly be nessicary but will help with debugging 
-        void Clear()
-        {
-            a = 0;
-            b = 0;
-            bus = 0;
-        }
 
         //Clear down the input/output queue if you want to start a program fresh.
         public void ClearInputOutput()
@@ -120,22 +120,15 @@ namespace Day9
             }
         }
 
-
         //** Opp codes **//
         //TODO: Proper logging 
-
-        //Opp codes are made up of indiviudal instructions for the fun of it.
 
         //01
         void OppSum(string param)
         {
-            //Console.WriteLine(String.Format("Excuting OppSum with params: {0}", param));
-            //Console.WriteLine("Writing to A");
-            ReadToReg(param[2], ref a);
-            //Console.WriteLine("Writing to B");
-            ReadToReg(param[1], ref b);
-            Sum();
-            Write(param[0]);
+            long a = Read(param[2]);
+            long b = Read(param[1]);
+            Write(param[0], a + b);
             pCounter += 1;
         }
 
@@ -143,21 +136,17 @@ namespace Day9
         void OppMultiply(string param)
         {
             //Console.WriteLine(String.Format("Excuting OppMultiply with params: {0}", param));
-            //Console.WriteLine("Writing to A");
-            ReadToReg(param[2], ref a);
-            //Console.WriteLine("Writing to B");
-            ReadToReg(param[1], ref b);
-            Multiply();
-            Write(param[0]);
+            long a = Read(param[2]);
+            long b = Read(param[1]);
+            Write(param[0], a * b);
             pCounter += 1;
         }
 
         //03
-        void OppInput()
+        void OppInput(string param)
         {
             //Console.WriteLine("Excuting OppInput");
-            ReadIn();
-            Write('0');
+            Write(param[2], ReadIn());
             pCounter += 1;
         }
 
@@ -165,9 +154,7 @@ namespace Day9
         void OppOutput(string param)
         {
             //Console.WriteLine("Excuting OppOutput");
-            //Console.WriteLine("Writing to A");
-            ReadToReg(param[2], ref a);
-            WriteAOut();
+            Output.Add(Read(param[2]));
             pCounter += 1;
         }
 
@@ -175,11 +162,9 @@ namespace Day9
         void OppJumpTrue(string param)
         {
             //Console.WriteLine(String.Format("Excuting OppJumpTrue with params: {0}", param));
-            //Console.WriteLine("Writing to A");
-            ReadToReg(param[2], ref a);
-            //Console.WriteLine("Writing to B");
-            ReadToReg(param[1], ref b);
-            //Shame I can't think of a way to do this with instructions 
+            long a = Read(param[2]);
+            long b = Read(param[1]);
+
             //Console.WriteLine(String.Format("Does a == 0?: {0}", a));
             if (a == 0)
             {
@@ -197,11 +182,9 @@ namespace Day9
         void OppJumpFalse(string param)
         {
             //Console.WriteLine(String.Format("Excuting OppJumpFalse with params: {0}", param));
-            //Console.WriteLine("Writing to A");
-            ReadToReg(param[2], ref a);
-            //Console.WriteLine("Writing to B");
-            ReadToReg(param[1], ref b);
-            //Shame I can't think of a way to do this with instructions 
+            long a = Read(param[2]);
+            long b = Read(param[1]);
+
             //Console.WriteLine(String.Format("Does a == 0?: {0}", a));
             if (a == 0)
             {
@@ -220,12 +203,9 @@ namespace Day9
         void OppLessThan(string param)
         {
             //Console.WriteLine(String.Format("Excuting OppLessThan with params: {0}", param));
-            //Console.WriteLine("Writing to A");
-            ReadToReg(param[2], ref a);
-            //Console.WriteLine("Writing to B");
-            ReadToReg(param[1], ref b);
-            LessThan();
-            Write(param[0]);
+            long a = Read(param[2]);
+            long b = Read(param[1]);
+            Write(param[0], a < b ? 1 : 0);
             pCounter += 1;
         }
 
@@ -233,12 +213,9 @@ namespace Day9
         void OppEquals(string param)
         {
             //Console.WriteLine(String.Format("Excuting OppEquals with params: {0}", param));
-            //Console.WriteLine("Writing to A");
-            ReadToReg(param[2], ref a);
-            //Console.WriteLine("Writing to B");
-            ReadToReg(param[1], ref b);
-            Equals();
-            Write(param[0]);
+            long a = Read(param[2]);
+            long b = Read(param[1]);
+            Write(param[0], a == b ? 1 : 0);
             pCounter += 1;
         }
 
@@ -247,9 +224,8 @@ namespace Day9
         void OppOffset(string param)
         {
             //Console.WriteLine(String.Format("Adjusting offset with params: {0}", param));
-            //Console.WriteLine("Writing to A");
-            ReadToReg(param[2], ref a);
-            AddAToOffset();
+            long a = Read(param[2]);
+            pOffset += a;
             pCounter += 1;
         }
 
@@ -264,72 +240,69 @@ namespace Day9
         //The instuctions that make up the OppCodes
 
         //IO
-        void ReadIn()
+        long ReadIn()
         {
-            while (!Input.TryTake(out bus))
+            long r;
+            while (!Input.TryTake(out r))
             {
-                //should grab the first item from the queu then break? 
+                //If we can't grab the input from the queue we'll wait
+                //We'll flag input as being needed to make working with the VM from outside easier 
+                InputRequired = true;
+                //Don't want it to keep setting InputRequired but I don't know if this is the best way of doing it.
+                Thread.Sleep(50);
             }
-        }
-
-        void WriteAOut()
-        {
-            //Console.WriteLine(String.Format("Program output at step {0}: {1}", pCounter, a));
-            Output.Add(a);
+            InputRequired = false;
+            return r;
         }
 
 
         //Read/Write Memory 
 
-        //Read To Register 
+        //Read
         //This instuctions has to work in versious modes so it switchs on them.
         
-        void ReadToReg(char mode, ref long reg)
+        long Read(char mode)
         {
-            switch (mode)
+            pCounter += 1;
+            return mode switch
             {
-                case '0': ReadPositionToReg(ref reg); break;
-                case '1': ReadImmediateToReg(ref reg); break;
-                case '2': ReadOffsetPositionToReg(ref reg); break;
-                default:
-                    Console.WriteLine(String.Format("Error: Unknown Read Mode {0}", mode.ToString()));
-                    run = false;
-                    break;
+                '0' => ReadPosition(),
+                '1' => ReadImmediate(),
+                '2' => ReadOffset(),
+                _ => throw new ArgumentException(message: "invalid Read Mode value"),
             };
         }
 
-        void ReadPositionToReg(ref long reg)
+        long ReadPosition()
         {
-            pCounter += 1;
             //Console.WriteLine(String.Format("Step {0}: Reading {1} from address {2}", pCounter, Memory[Memory[pCounter]], Memory[pCounter]));
-            reg = Memory[Memory[pCounter]];
+            return Memory[Memory[pCounter]];
         }
 
        
-        void ReadImmediateToReg(ref long reg)
+        long ReadImmediate()
         {
-            pCounter += 1;
             //Console.WriteLine(String.Format("Step {0}: Reading {1} from address {0}", pCounter, Memory[pCounter]));
-            reg = Memory[pCounter];
+            return Memory[pCounter];
         }
 
         //Could do this as the ReadPostionToReg with an optional paramater but I like this being clear.
-        void ReadOffsetPositionToReg(ref long reg)
+        long ReadOffset()
         {
-            pCounter += 1;
             //Console.WriteLine(String.Format("Step {0}: Reading {1} from address {2}", pCounter, Memory[Memory[pCounter]], Memory[pCounter]));
-            reg = Memory[Memory[pCounter + pOffset]];
+            return Memory[Memory[pCounter + pOffset]];
         }
 
         //Write to Memoery 
         //This instuctions has to work in versious modes so it switchs on them.
-        void Write(char mode)
+        void Write(char mode, long val)
         {
+            pCounter += 1;
             switch (mode)
             {
-                case '0': WriteToPosition(); break;
-                case '1': WriteToImmediate(); break;
-                case '2': WriteToOffsetPosition(); break;
+                case '0': WriteToPosition(val); break;
+                //case '1': WriteToImmediate(val); break;
+                case '2': WriteToOffsetPosition(val); break;
                 default:
                     Console.WriteLine(String.Format("Error: Unknown Write Mode {0}", mode.ToString()));
                     run = false;
@@ -337,62 +310,23 @@ namespace Day9
             };
         }
 
-        void WriteToPosition()
+        void WriteToPosition(long val)
         {
-            pCounter += 1;
             //Console.WriteLine(String.Format("Step {0}: Writing {1} to memory to address {2}", pCounter, bus, Memory[pCounter]));
-            Memory[Memory[pCounter]] = bus;
+            Memory[Memory[pCounter]] = val;
         }
 
+        //This shouldn't be used? 
+        //void WriteToImmediate(long val)
+        //{
+        //    //Console.WriteLine(String.Format("Step {0}: Writing {1} to address {0}", pCounter, bus));
+        //    Memory[pCounter] = val;
+        //}
 
-        void WriteToImmediate()
+        void WriteToOffsetPosition(long val)
         {
-            pCounter += 1;
-            //Console.WriteLine(String.Format("Step {0}: Writing {1} to address {0}", pCounter, bus));
-            Memory[pCounter] = bus;
-        }
-
-        void WriteToOffsetPosition()
-        {
-            pCounter += 1;
             //Console.WriteLine(String.Format("Step {0}: Writing {1} to memory to address {2}", pCounter, bus, Memory[pCounter]));
-            Memory[Memory[pCounter + pOffset]] = bus;
+            Memory[Memory[pCounter + pOffset]] = val;
         }
-
-
-        //Pointer intruscitons 
-        void AddAToOffset()
-        {
-            pOffset += a;
-        }
-
-        //Opperations 
-        void Sum()
-        {
-            //Console.WriteLine(String.Format("Sum A + B: {0} + {1} = {2}",a,b,a + b));
-            bus = a + b;
-        }
-
-        void Multiply()
-        {
-            //Console.WriteLine(String.Format("Multiply A * B: {0} * {1} = {2}", a, b, a * b));
-            bus = a * b;
-        }
-
-        void LessThan()
-        {
-            //Console.WriteLine(String.Format("Equals A < B: {0} == {1} = {2}", a, b, (a < b).ToString()));
-            bus = a < b ? 1 : 0;
-        }
-
-        void Equals()
-        {
-            //Console.WriteLine(String.Format("Equals A = B: {0} == {1} = {2}", a, b, (a == b).ToString()));
-            bus = a == b ? 1 : 0;
-        }
-
-
-
-
     }
 }
--- Day9/Program.cs
+++ Day9/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,41 +8,47 @@ namespace Day9
 {
     class Program
     {
-        static long[] program;
+        static List<long[]> programs;
         static IntCodeVM VM;
         static void Main()
         {
-            string[] input = File.ReadLines(@"./Programs/CopySelfTest.txt").First().Split(',');
-            program = Array.ConvertAll(input, long.Parse);
+            //Parse programs to be loaded into the VM.
+            string[] input = File.ReadLines(@"./Programs/Day9Input.txt").First().Split(',');
+            programs = new List<long[]>();
+            programs.Add(Array.ConvertAll(input, long.Parse));
+            input = File.ReadLines(@"./Programs/Day5Diagnostic.txt").First().Split(',');
+            programs.Add(Array.ConvertAll(input, long.Parse));
+            //This currently isn't working
+            input = File.ReadLines(@"./Programs/CopySelfTest.txt").First().Split(',');
+            programs.Add(Array.ConvertAll(input, long.Parse));
 
-
-            VM = new IntCodeVM(program);
+            VM = new IntCodeVM(programs);
 
             while (true)
             {
-                VM.LoadProgram();
-                Task vmTask = Task.Run(() => VM.Execute());
+                //clear down input and outputs to be on the same side
+                VM.ClearInputOutput();
+                //Might want to get fancy with this in futurue and have a diconatry 
+                Console.WriteLine("Program to run (zero indexed)?");
+                int program = int.Parse(Console.ReadLine());
+                VM.LoadProgram(program);
 
-                //Console.WriteLine("Intial Input: ");
-                //VM.Input.Add(long.Parse(Console.ReadLine()));
+                Task vmTask = Task.Run(() => VM.Execute());
 
                 long outPut;
                 while (!vmTask.IsCompleted)
                 {
-                    //if (VM.Output.TryTake(out outPut))
-                    //{
-                    //    Console.WriteLine(String.Format("Output: {0}", outPut.ToString()));
-                    //}
+                    if(VM.InputRequired)
+                    {
+                        Console.WriteLine("Input Required: ");
+                        VM.AddInput(long.Parse(Console.ReadLine()));
+                    }
                 }
                 while (VM.Output.TryTake(out outPut))
                 {
                     Console.WriteLine(String.Format("Output: {0}", outPut.ToString()));
                 }
-                Console.WriteLine("Program Finsihed");
-
-                //copy to self not working.
-                Console.WriteLine("Press key to run again");
-                Console.ReadKey();
+                Console.WriteLine("Program Finished");
             }
             //Console.ReadLine();
         }
