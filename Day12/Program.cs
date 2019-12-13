c99cff2aec304eaff3e3c6616f2d73296a718483
Timothy Griffiths <timgriffiths01@gmail.com>
2019-12-13 12:19:50 +0000

    Day12: Just an implemeantion of other peoples ideas

--- Day12/Program.cs
+++ Day12/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Numerics;
+using System.Runtime;
+using System.Text;
 
 namespace Day12
 {
@@ -13,7 +15,7 @@ namespace Day12
         //<x=12, y=-14, z=-4>
         //<x=9, y=5, z=-6>
         //<x=-1, y=-4, z=9>
-        
+
         //Test1
         //<x=-1, y=0, z=2>
         //<x=2, y=-10, z=-7>
@@ -34,13 +36,14 @@ namespace Day12
             //test1
             //Moon[] moons = new Moon[]
             //{
-            //    new Moon('I',new Vector3(-1,0,2)),
-            //    new Moon('E',new Vector3(2,-10,-7)),
-            //    new Moon('G',new Vector3(4,-8,8)),
-            //    new Moon('C',new Vector3(3,5,-1))
+            //    new Moon('I', new Vector3(-1, 0, 2)),
+            //    new Moon('E', new Vector3(2, -10, -7)),
+            //    new Moon('G', new Vector3(4, -8, 8)),
+            //    new Moon('C', new Vector3(3, 5, -1))
             //};
 
             //int steps = 1000;
+            // 2772 is the steps for the test
             moons = ProcessOrbit(moons);
 
 
@@ -59,90 +62,171 @@ namespace Day12
         {
             int mLen = moons.Length;
 
-            //Vector3[] staringP = new Vector3[mLen];
+            double minx = 0;
+            double miny = 0;
+            double minz = 0;
 
-            //for (int m = 0; m < mLen; m++)
-            //{
-            //    staringP[m] = moons[m].Postion;
-            //}
-
-            //Itterate the steps
-            for (double i = 0; i < steps; i++)
+            for (int a = 0; a < 3; a++)
             {
-                
-                //Check each moon
-                for (int m1 = 0; m1 < mLen; m1++)
-                {
-                    Moon currentMoon = moons[m1];
-                    //Againt every other moon
-                    for (int m2 = m1 + 1; m2 < mLen; m2++)
-                    {
-                        //Seems it'll run the loop first time regardless so need to break if we're at the end.
-                        if (m2 >= mLen)
-                        {
-                            break;
-                        }
-
-                        Moon compMoon = moons[m2];
-                        ComperMoons(moons[m1], moons[m2]);
-                    }
-                }
-
-                Console.WriteLine(String.Format("Step: {0}", i.ToString()));
-                float enargy = 0;
                 foreach (Moon m in moons)
                 {
-                    m.ApplyVelocity();
-                    enargy += m.GetEnargy();
-                    //Console.WriteLine(m.ToString());
+                    m.Reset();
                 }
 
-                //Need do this just on each axis then get the Losest common mulitplier. Again stolen from other people!
-
-                bool match = true;
-                for (int m3 = 0; m3 < mLen; m3++)
+                //Itterate the steps
+                for (double i = 0; i < steps; i++)
                 {
-                    //Can't take credit for this.
-                    //Logically you don't have to check postion, we just have to know if the volcity is zero
-                    match &= moons[m3].Velocity == Vector3.Zero;
-                }
 
-                if(match)
-                {
-                    //Can't take credit for this either
-                    //Planets move in symetrical cycles which means their volocity will reach zero at half the number of steps it'll take to get back to their orignal postion.
-                    Console.WriteLine(String.Format("Back to orignal state at step {0}", ((i+1) * 2).ToString()));
-                    break;
-                }
+                    //Check each moon
+                    for (int m1 = 0; m1 < mLen; m1++)
+                    {
+                        //Againt every other moon
+                        for (int m2 = m1 + 1; m2 < mLen; m2++)
+                        {
+                            //Seems it'll run the loop first time regardless so need to break if we're at the end.
+                            if (m2 >= mLen)
+                            {
+                                break;
+                            }
 
+                            if (a == 0)
+                            {
+                                ComperMoonsX(moons[m1], moons[m2]);
+                            }
+                            else if (a == 1)
+                            {
+                                ComperMoonsY(moons[m1], moons[m2]);
+                            }
+                            else if (a == 2)
+                            {
+                                ComperMoonsZ(moons[m1], moons[m2]);
+                            }
+                        }
+                    }
+
+                    //Console.WriteLine(String.Format("Step: {0}", i.ToString()));
+                    //float enargy = 0;
+                    foreach (Moon m in moons)
+                    {
+                        m.ApplyVelocity();
+                        //enargy += m.GetEnargy();
+                        //Console.WriteLine(m.ToString());
+                    }
+
+                    //Need do this just on each axis then get the Losest common mulitplier. Again stolen from other people!
+
+                    bool match = true;
+                    for (int m3 = 0; m3 < mLen; m3++)
+                    {
+                        //Can't take credit for this.
+                        //Logically you don't have to check postion, we just have to know if the volcity is zero
+                        match &= moons[m3].Velocity == Vector3.Zero;
+                    }
+
+                    if (match)
+                    {
+                        //Can't take credit for this either
+                        //Planets move in symetrical cycles which means their volocity will reach zero at half the number of steps it'll take to get back to their orignal postion.
+                       
+                        if (a == 0)
+                        {
+                            minx = i + 1;
+                        }
+                        else if (a == 1)
+                        {
+                            miny = i + 1;
+                        }
+                        else if (a == 2)
+                        {
+                            minz = i + 1;
+                        }
+                        break;
+                    }
+                }
             }
+
+            Console.WriteLine(minx.ToString());
+            Console.WriteLine(miny.ToString());
+            Console.WriteLine(minz.ToString());
+
+            Console.WriteLine((LCM(minx, LCM(miny, minz))*2).ToString());
 
             return moons;
         }
 
+        static double GCD(double a, double b)
+        {
+            if (a % b == 0) return b;
+            return GCD(b, a % b);
+        }
+
+        static double LCM(double a, double b)
+        {
+            return a * b / GCD(a, b);
+        }
+
         static void ComperMoons(Moon moonA, Moon moonB)
+        {
+            ComperMoonsX(moonA, moonB);
+            ComperMoonsY(moonA, moonB);
+            ComperMoonsZ(moonA, moonB);
+        }
+
+        //Axises are indepent so we should split this out really 
+        //Just doing one for testing now
+        static void ComperMoonsX(Moon moonA, Moon moonB)
         {
             //need three way logic here for ==s
             int vx = 0;
-            int vy = 0;
-            int vz = 0;
+          
             vx = moonA.Postion.X < moonB.Postion.X ? 1 : -1;
             vx = moonA.Postion.X == moonB.Postion.X ? 0 : vx;
+
+
+            Vector3 va = new Vector3(vx, 0, 0);
+
+            moonA.AddVelocity(va);
+
+            Vector3 vb = new Vector3(vx * -1, 0, 0);
+
+            moonB.AddVelocity(vb);
+        }
+
+        static void ComperMoonsY(Moon moonA, Moon moonB)
+        {
+            //need three way logic here for ==s
+
+            int vy = 0;
 
             vy = moonA.Postion.Y < moonB.Postion.Y ? 1 : -1;
             vy = moonA.Postion.Y == moonB.Postion.Y ? 0 : vy;
 
-            vz = moonA.Postion.Z < moonB.Postion.Z ? 1 : -1;
-            vz = moonA.Postion.Z == moonB.Postion.Z ? 0 : vz;
 
-            Vector3 va = new Vector3(vx, vy, vz);
+            Vector3 va = new Vector3(0, vy, 0);
 
             moonA.AddVelocity(va);
 
-            Vector3 vb = new Vector3(vx * -1, vy * -1, vz * -1);
+            Vector3 vb = new Vector3(0, vy * -1, 0);
 
             moonB.AddVelocity(vb);
         }
+
+        static void ComperMoonsZ(Moon moonA, Moon moonB)
+        {
+            //need three way logic here for ==s
+            int vz = 0;
+            vz = moonA.Postion.Z < moonB.Postion.Z ? 1 : -1;
+            vz = moonA.Postion.Z == moonB.Postion.Z ? 0 : vz;
+
+            Vector3 va = new Vector3(0, 0, vz);
+
+            moonA.AddVelocity(va);
+
+            Vector3 vb = new Vector3(0, 0, vz * -1);
+
+            moonB.AddVelocity(vb);
+        }
+
     }
 
     class Moon
@@ -151,10 +235,13 @@ namespace Day12
         public Vector3 Postion { get; protected set; }
         public Vector3 Velocity { get; protected set; }
 
+        public Vector3 StartingPos { get; protected set; }
+
         public Moon(char id, Vector3 startingPos)
         {
             ID = id;
             Postion = startingPos;
+            StartingPos = startingPos;
             Velocity = Vector3.Zero;
         }
 
@@ -179,7 +266,13 @@ namespace Day12
         public override string ToString()
         {
             return String.Format("[{6}] pos=<x=-{0}, y=  {1}, z= {2}>, vel=<x= {3}, y= {4}, z= {5}>"
-                , Postion.X, Postion.Y, Postion.Z, Velocity.X, Velocity.Y , Velocity.Z, ID.ToString());
+                , Postion.X, Postion.Y, Postion.Z, Velocity.X, Velocity.Y, Velocity.Z, ID.ToString());
+        }
+
+        public void Reset()
+        {
+            Postion = StartingPos;
+            Velocity = Vector3.Zero;
         }
     }
 
